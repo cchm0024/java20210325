3326e8ab7100cbc49f683d40a75421319e748132
cchm0024 <cchm0024@naver.com>
2021-03-25 16:44:02 +0900

    20210325

---
+++ src/p02/textbook/CharExample.java
@@ -0,0 +1,13 @@
+package p02.textbook;
+
+public class CharExample {
+ public static void main(String[] args) {
+	char c1 = 'A'; // 문자를 직접 저장
+	char c2 = 65;  // 10진수로 저장
+	char c3 = '\u0041'; // 16진수로 저장
+	
+	char c4 = '가';  // 문자를 직접 저장
+	char c5 = 44032; // 10진수로 저장
+	char c6 = '\uac00'; //16진수로저장
+}
+}
