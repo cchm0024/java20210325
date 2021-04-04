fc83a1a7b3974407291456f9472be2a405771853
cchm0024 <cchm0024@naver.com>
2021-04-05 01:05:17 +0900

    20210402

--- src/p05/textbook/ArrayCreateByValueListExample2.java
+++ src/p05/textbook/ArrayCreateByValueListExample2.java
@@ -3,23 +3,21 @@ package p05.textbook;
 public class ArrayCreateByValueListExample2 {
 	public static void main(String[] args) {
 		int[] scores;
-		scores = new int[] {83,90,87};
+		scores = new int[]{83,90,87};
 		int sum1 = 0;
-		for(int i = 0; i<3; i++) {
+		for(int i=0; i<=2; i++) {
 			sum1 += scores[i];
 		}
-		System.out.println("종합 :' + sum2");
+		System.out.println("총합" + sum1);
 		
-//		리턴된 종합을 sum2에 저장
-		
-		int sum2 = add(new int[] {83, 90,  87});
-		System.out.println("종합 : "+ sum2);
-		System.out.println();
+		int sum2 = add(new int[]{83, 90, 87});
+		System.out.println("총합" + sum2);
+		}
+	public static int add(int[] scores) {
+		int sum = 0;
+		for(int i=0; i<=2; i++) {
+			sum += scores[i];
+		}
+		return sum;
 	}
-public static int add(int[] scores) {
-	int sum = 0;
-	for(int i=0; i<3; i++) {
-		sum += scores[i];
 	}
-	return sum;
-}
