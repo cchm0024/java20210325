dd6ac52337da8b1414a55f699730307282b9f1bc
cchm0024 <cchm0024@naver.com>
2021-04-07 01:06:57 +0900

    20210407

--- src/p6/lecyure/p3constructor/A03ThisKeyword.java
+++ src/p6/lecyure/p3constructor/A03ThisKeyword.java
@@ -2,6 +2,7 @@ package p6.lecyure.p3constructor;
 
 public class A03ThisKeyword {
 		//	this : 앞으로 만들어질 객체의 참조변수를 대신하는 키워드
+		//         필드임을 명시하기 위하여
 	public static void main(String[] args) {
 		Tv tv1 = new Tv("samsung");
 		System.out.println(tv1.name);
