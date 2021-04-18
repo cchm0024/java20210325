dbe36c2ea6a139c111023692c6755720c102bd48
cchm0024 <cchm0024@naver.com>
2021-04-18 23:49:34 +0900

    20210408

--- src/p8/textbook/s080701/MyClassA.java
+++ src/p8/textbook/s080701/MyClassA.java
@@ -1,7 +1,9 @@
 package p8.textbook.s080701;
 
-public class MyClassA implements MyInterFace{
+public class MyClassA implements MyInterFace {
 @Override
 public void method1() {
 	// TODO Auto-generated method stub
 	System.out.println("myclassa - method1");
+}
+}
